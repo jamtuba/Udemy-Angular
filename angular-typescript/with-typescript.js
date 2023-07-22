88103cce708e0f9035ea98f00fbd06aa158097ec
Janus Mogensen <jamtuba@gmail.com>
2023-07-22 11:57:02 +0200

    First commit

---
+++ angular-typescript/with-typescript.js
@@ -0,0 +1,5 @@
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+var result = addTwoNumbers(2, 5);
+console.log(result);
---
+++ angular-typescript/with-typescript.ts
@@ -0,0 +1,7 @@
+function addTwoNumbers(a: number, b: number) {
+    return a + b;
+}
+
+const result = addTwoNumbers(2, 5);
+
+console.log(result);
