8fabe8fe500815b4226990ad07d7f60c2202a34a
Janus Mogensen <jamtuba@gmail.com>
2023-08-04 18:14:07 +0200

    Assignment 2

--- basics-assignment-2-start/src/app/app.component.ts
+++ basics-assignment-2-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  username = '';
+
+  updateUsername(event: Event) {
+    this.username = (<HTMLInputElement>event.target).value;
+  }
+
+  onResetUsername(){
+    this.username = '';
+  }
 }
