f6c312af551a5a5b9b25967d967ced6dff12ff57
Janus Mogensen <jamtuba@gmail.com>
2023-08-08 11:35:24 +0200

    Lesson 60

--- big-project-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ big-project-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2018/08/29/19/03/steak-3640560_1280.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2018/08/29/19/03/steak-3640560_1280.jpg')
+  ];
 }
